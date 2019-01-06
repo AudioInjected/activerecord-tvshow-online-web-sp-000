31faea4695ea0a144c2a364027ba314c8686cb7a
Rafael Batista <rafaelb24@yahoo.com>
2019-01-06 18:04:42 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,9 @@ class Show < ActiveRecord::Base
   def self.highest_rating 
     Show.maximum(:rating)
   end 
+  
+  def self.most_popular_show 
+    rating = self.highest_rating 
+    binding.pry 
+  end 
 end 
